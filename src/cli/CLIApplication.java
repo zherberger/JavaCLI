5cf008e37e152967b7cb38f35579398b040f2508
zherberger <zherberger@gmail.com>
2019-10-13 21:35:14 -0400

    Getting there. Still working on background processes.

--- src/cli/CLIApplication.java
+++ src/cli/CLIApplication.java
@@ -1,21 +1,32 @@
 package cli;
 
+import java.util.Vector;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class CLIApplication extends Application implements ProcessListener {
 	ScrollPane root;
 	VBox outputBox;
 	TextField inputField;
+	Scene scene;
+	EventHandler<KeyEvent> commandHandler;
+	ProcessThread currentProcess;
+	Vector<ProcessThread> backgroundProcesses;
+	Vector<String> history;
+	int historyIndex;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -23,19 +34,47 @@ public class CLIApplication extends Application implements ProcessListener {
 	
 	@Override
 	public void start(Stage primaryStage) {
+		backgroundProcesses = new Vector<ProcessThread>();
 		outputBox = new VBox();
+		history = new Vector<String>();
+		historyIndex = 0;
 		
-		TextField inputField = new TextField();
+		inputField = new TextField();
 		inputField.setStyle("-fx-background-color: rgba(0, 0, 0, 0);"); // transparent background. Makes it not look like an obvious TextField.
 		inputField.requestFocus();
 		
-		inputField.setOnKeyPressed(e -> {
-			if(e.getCode().equals(KeyCode.ENTER)) { 
-				appendOutput("$" + inputField.getText());
-				runCommand(inputField.getText());
-				inputField.setText("");
+		// The big one! This'll determine what happens based on the input to the inputField.
+		commandHandler = new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent e) {
+				
+				// Was a command entered?
+				if(e.getCode().equals(KeyCode.ENTER)) { 
+					appendLastInput();
+					runCommand(inputField.getText());
+					inputField.setText("");
+				}
+				
+				// Maybe the up arrow was pressed to request a recent command?
+				else if(e.getCode().equals(KeyCode.UP) && historyIndex != -1) {
+					inputField.setText(history.get(historyIndex--));
+				}
+				
+				// Pressing the down arrow should revert to a "less previous" command
+				else if(e.getCode().equals(KeyCode.DOWN)) {
+					if(historyIndex == history.size())
+						inputField.setText("");
+					
+					else inputField.setText(history.get(++historyIndex));
+				}
+				
+				// Ctrl+C to cancel the current process
+				else if(e.getCode().equals(KeyCode.C) && e.isControlDown() && currentProcess != null) {
+					currentProcess.kill();
+				}
 			}
-		});
+		};
+		
+		inputField.setOnKeyPressed(commandHandler);
 		
 		Label promptLabel = new Label("$");
 		promptLabel.setLabelFor(inputField);
@@ -43,8 +82,11 @@ public class CLIApplication extends Application implements ProcessListener {
 		
 		HBox inputBox = new HBox();
 		inputBox.getChildren().addAll(promptLabel, inputField);
+		inputField.setPrefWidth(400);
+		System.out.println(inputField.getWidth());
 		
 		BorderPane cli = new BorderPane();
+		BorderPane.setMargin(inputBox, new Insets(0, 0, 0, 8));
 		cli.setCenter(outputBox);
 		cli.setBottom(inputBox);
 		BorderPane.setMargin(outputBox, new Insets(0, 8, 0, 8));
@@ -54,36 +96,86 @@ public class CLIApplication extends Application implements ProcessListener {
 		root.setPrefHeight(400);
 		root.setContent(cli);
 		
-		Scene scene = new Scene(root);
-		primaryStage.sizeToScene();
+		scene = new Scene(root);
+		
+		scene.setOnMouseClicked(e -> {
+			inputField.requestFocus(); // Clicked anywhere else? Too bad, you gotta keep typing
+		});
+		
+		// Might be good to have a "history" feature
+		scene.setOnKeyPressed(e -> {
+			
+		});
+		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
 	private void runCommand(String command) {
+		history.add(command);
+		historyIndex = history.size() - 1;
+		
 		String[] parts = command.split(" ");
+		ProcessThread processThread;
 		
 		switch(parts[0]) {
-			// Builtins, then:
-			default: {
-				ProcessThread processThread = new ProcessThread(this, command);
+			case "killall": {
+				for(ProcessThread pt : backgroundProcesses)
+					pt.kill();
 				
-				if(!parts[parts.length - 1].equals("&")) { // emulate "run in background" feature of modern shells
-
+				break;
+			}
+			
+			case "history": {
+				for(int i = 0; i < history.size(); i++) {
+					appendOutput(i + ": " + history.get(i));
 				}
-			}	 
+				
+				break;
+			}
+			
+			default: {
+				if(!parts[parts.length - 1].equals("&")) { // emulate "run in background" feature of modern shells
+					currentProcess = new ProcessThread(this, command);
+					
+//					inputField.setOnKeyPressed(e -> {
+//						if(e.getCode().equals(KeyCode.C)) {
+//							if(e.isControlDown()) {
+//								processThread.kill();
+//								inputField.setOnKeyPressed(commandHandler);
+//							}
+//						}
+//					});
+				}
+				
+				else {
+					String commandWithoutAmpersand = "";
+					
+					for(int i = 0; i < parts.length - 1; i++)
+						commandWithoutAmpersand += parts[i] + " ";
+					
+					backgroundProcesses.add(new ProcessThread(this, commandWithoutAmpersand));
+				}
+			}
 		}
 	}
 	
 	private void scrollToBottom() {
-		System.out.println(root.getVvalue());
-		
-		if(root.getVvalue() == 1.0)
-			root.setVvalue(1.0);
+		root.setVvalue(1.5);
 	}
 	
-	// Appending outputBox/error is gonna be done one line at a time, for simplicity's sake.
-
+	private void appendLastInput() {
+		Label dollarSign = new Label("$");
+		TextField lastInput = new TextField(inputField.getText());
+		lastInput.setStyle("-fx-background-color: rgba(0, 0, 0, 0);");
+		lastInput.setEditable(false);
+		HBox.setMargin(dollarSign, new Insets(5, 0, 0, 0));
+		HBox line = new HBox();
+		line.getChildren().addAll(dollarSign, lastInput);
+		outputBox.getChildren().add(line);
+		scrollToBottom();
+	}
+	
 	@Override
 	public void appendOutput(String output) {
 		Label outputLabel = new Label(output);
@@ -94,6 +186,7 @@ public class CLIApplication extends Application implements ProcessListener {
 	@Override
 	public void appendError(String error) {
 		Label errorLabel = new Label(error);
+		errorLabel.setTextFill(Color.RED);
 		outputBox.getChildren().add(errorLabel);
 		scrollToBottom();
 	}
--- src/cli/ProcessThread.java
+++ src/cli/ProcessThread.java
@@ -11,17 +11,15 @@ import java.io.IOException;
 public class ProcessThread implements Runnable {
 
 	private ProcessListener cli;
+	private Process process;
 	private String command;
 	private Runtime runtime;
-	private StringBuffer stdout;
-	private StringBuffer stderr;
+	private boolean killed;
 	
 	public ProcessThread(ProcessListener cli, String command) {
 		this.cli = cli;
 		this.command = command;
 		runtime = Runtime.getRuntime();
-		stdout = new StringBuffer();
-		stderr = new StringBuffer();
 		
 		// This just feels so CLEAN. Thanks Dr. LaRue.
 		new Thread(this).start();
@@ -30,40 +28,42 @@ public class ProcessThread implements Runnable {
 	@Override
 	public void run() {
 		try {
-			Process process = runtime.exec(command);
+			process = runtime.exec(command);
 			
 			InputStream inputStream = process.getInputStream();
 			InputStreamReader isr = new InputStreamReader(inputStream);
 			InputStream errorStream = process.getErrorStream();
 			InputStreamReader esr = new InputStreamReader(errorStream);
 			
-			// I got this next part from the tutorial page on the assignment: http://www.avajava.com/tutorials/lessons/how-do-i-run-another-application-from-java.html?page=2
-			// Processes can have continuous output, so it makes sense that a buffer-type object is needed.
-			
-			int n1;
+			// I got the basics for this next part from the tutorial page on the assignment: http://www.avajava.com/tutorials/lessons/how-do-i-run-another-application-from-java.html?page=2
+			// If a process had continuous output, it would get stuck appending to a StringBuffer. I went with a solution that just involves appending each line encountered to the CLI.
 			char[] c1 = new char[1024];
 			
-			while((n1 = isr.read(c1)) > 0) {
-				// This may seem intentionally obfuscated, but it's super concise.
+			while(isr.read(c1) > 0 && !killed) {
+				// This next line may seem intentionally obfuscated, but it's super concise.
 				// Java's functional interface allows us to specify instances of an interface in this fashion, as long as that interface has only one method.
 				// The empty parentheses () just mean there are no parameters, as run() doesn't have any for a Runnable.
 				// Sure beats defining an inner class every time.
+				
+				// This is analogous to SwingUtilities.invokeLater from Network Programming.
+				// The action taking place in the Application needs to be in the same thread as the Application. Makes sense.
 				Platform.runLater(() -> cli.appendOutput(new String(c1)));
 			}
 			
-			int n2;
 			char[] c2 = new char[1024];
 			
-			while((n2 = esr.read(c2)) > 0) {
+			while(esr.read(c2) > 0 & !killed) {
 				Platform.runLater(() -> cli.appendError(new String(c2)));
 			}
-			
-			System.out.println("Exiting...");
 		}
 		
 		catch(IOException e) {
 			Platform.runLater(() -> cli.appendError("IOException encountered: " + e.getMessage()));
 		}
+	}
+	
+	public void kill() {
+		killed = true;
 	}
 
 	public String getCommand() {
